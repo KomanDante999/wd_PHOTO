20c05b7e367e46eaaf0ad391f03dded30ff9368f
KomanDante999 <Dreftware@yandex.ru>
2023-02-27 21:02:15 +0700

    animation burger

--- src/js/components/burger-menu.js
+++ src/js/components/burger-menu.js
@@ -1,91 +1,42 @@
-const { pause } = require("browser-sync")
 
 class BurgerMenu {
 
   constructor(params) {
 
-    this.widthWindow = document.documentElement.clientWidth
-    this.$menu = document.querySelector(`#${params.menu}`)
     this.$btnOpen = document.querySelector(`#${params.btnOpen}`)
     this.$btnClose = document.querySelector(`#${params.btnClose}`)
     this.$wrapList = document.querySelector(`#${params.wrapList}`)
     this.$list = document.querySelector(`#${params.list}`)
+    this.$items = document.querySelectorAll(`.${params.item}`)
     this.$links = document.querySelectorAll(`.${params.link}`)
     this.$contactPhone = document.querySelector(`#${params.contactPhone}`)
 
-    var tlSetUnwrapMenu = gsap.timeline({paused: true})
-    tlSetUnwrapMenu
-      .set([this.$btnOpen, this.$btnClose, this.$contactPhone], {dislay: 'none'})
-
-    var tlSetBurgerMenu = gsap.timeline({paused: true})
-    tlSetBurgerMenu
-      .set([this.$btnOpen, this.$btnClose, this.$contactPhone], {dislay: 'none'})
-
-    this.setAttributes()
+    this.tlOpenBurger = gsap.timeline({paused: true})
+      .set([this.$btnClose, this.$contactPhone], {display: 'block', opacity: 0})
+      .set(this.$wrapList, {display: 'flex'})
+      .set(this.$items, { opacity: 0})
+      .to(this.$wrapList, {opacity: 1, duration: 0.3})
+      .fromTo(this.$items, {y: 100, x: 100}, {y: 0, x: 0, opacity: 1, duration: 0.3})
+      .fromTo(this.$btnClose, {y: -100}, {y: 0, opacity: 1, duration: 0.3})
+      .fromTo(this.$contactPhone, {y: 100}, {y: 0, opacity: 1, duration: 0.3})
 
     // events
-    window.addEventListener('resize', () => {
-      this.widthWindow = document.documentElement.clientWidth
-      this.setAttributes()
-    })
-
     this.$btnOpen.addEventListener('click', () => {
-      this.menuOpen()
+      this.tlOpenBurger.play()
     })
     this.$btnClose.addEventListener('click', () => {
-      this.menuClose()
+      this.tlOpenBurger.reverse()
     })
 
     for (const link of this.$links) {
       link.addEventListener('click', () => {
-        this.menuClose()
+        this.tlOpenBurger.reverse()
       })
     }
 
     this.$contactPhone.addEventListener('click', () => {
-      this.menuClose()
+      this.tlOpenBurger.reverse()
     })
 
-  }
-
-  menuOpen() {
-    this.$btnOpen.classList.add('is-hidden')
-    this.$wrapList.classList.remove('is-hidden')
-    this.$wrapList.classList.add('flex', 'flex-column')
-    this.$btnClose.classList.remove('is-hidden')
-  }
-
-  menuClose() {
-    this.$btnOpen.classList.remove('is-hidden')
-    this.$wrapList.classList.remove('flex', 'flex-column')
-    this.$wrapList.classList.add('is-hidden')
-    this.$btnClose.classList.add('is-hidden')
-  }
-
-  setUnwrapMenu() {
-    this.$wrapList.classList.remove('is-hidden', 'flex', 'flex-column')
-    this.$list.classList.add('grid', 'grid-menu')
-    this.$list.classList.remove('flex', 'flex-column')
-    this.$btnOpen.classList.add('is-hidden')
-    this.$btnClose.classList.add('is-hidden')
-    this.$contactPhone.classList.add('is-hidden')
-  }
-
-  setBurgerMenu() {
-    this.$wrapList.classList.add('is-hidden')
-    this.$list.classList.remove('grid', 'grid-menu')
-    this.$list.classList.add('flex', 'flex-column')
-    this.$btnOpen.classList.remove('is-hidden')
-    this.$btnClose.classList.add('is-hidden')
-    this.$contactPhone.classList.remove('is-hidden')
-  }
-
-  setAttributes() {
-    if (this.widthWindow > 520) {
-      this.setUnwrapMenu()
-    } else {
-      this.menuClose()
-      this.setBurgerMenu()
-    }
   }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 
 const burgerMenu = new BurgerMenu({
-  menu: 'burger-menu',
   btnOpen: 'burger-menu__btn-open',
   btnClose: 'burger-menu__btn-close',
   wrapList: 'burger-menu__wrap-list',
   list: 'burger-menu__list',
+  item: 'menu__item',
   link: 'menu__link',
   contactPhone: 'burger-menu__contact-phone',
 });
